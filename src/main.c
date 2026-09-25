f984e03a95414990dbf9be4050facb4830a30fc0
Vladyslav <lazyumbreon7@gmail.com>
2026-09-25 21:12:18 +0300

    Added dinosaur metasprite, basic animation test.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <gb/gb.h>
-#include <stdio.h>
+#include <gb/metasprites.h>
+#include "dinosaur_sprites.h"
 
-void main()
-{
-	printf("Testificate.");
+void main() {
+
+    set_sprite_data(0, 16, dinosaur_tiles);
+    SHOW_SPRITES;
+    DISPLAY_ON;
+    while(1)
+    {
+    	move_metasprite(dinosaur_lleg, 0, 0, 80, 76);
+    	delay(1000);
+    	move_metasprite(dinosaur_rleg, 0, 0, 80, 76);
+    	delay(1000);
+        wait_vbl_done();
+    }
+
 }
